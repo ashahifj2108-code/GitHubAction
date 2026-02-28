9b629ceead242fb0761b4f03d0e456fdc0008438
Shahif <ashahifj2108@gmail.com>
2026-02-28 11:51:23 +0530

    Test Calculator workflow Files Updated v3

--- Test_Calculator.py
+++ Test_Calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .Calculator import add, subtract, multiply, divide
+from Calculator import add, subtract, multiply, divide
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
