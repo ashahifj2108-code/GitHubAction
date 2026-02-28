e9b25579a05e5cf42f950af3df61ce709bb83729
Shahif <ashahifj2108@gmail.com>
2026-02-28 11:32:50 +0530

    Test Calculator workflow Files

---
+++ Test_Calculator.py
@@ -0,0 +1,24 @@
+import pytest
+
+from .Calculator import add, subtract, multiply, divide
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(0, 1) == -1
+    assert subtract(-1, -1) == 0    
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-1, 1) == -1
+    with pytest.raises(ValueError):
+        divide(5, 0)
+
